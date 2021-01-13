7f27e607d01fd4e65e1c06bf7ce43b3236079be3
andron13 <andron13@gmail.com>
2021-01-13 21:34:18 +0100

    added put and delete request

--- hello-web-servlett/src/main/java/de/telran/hellowebservlett/JsonName.java
+++ hello-web-servlett/src/main/java/de/telran/hellowebservlett/JsonName.java
@@ -2,10 +2,12 @@ package de.telran.hellowebservlett;
 
 public class JsonName {
     public String token;
+    public String secondToken;
 
     //DTO Data transfer Object
     public JsonName(String string) {
         token = string;
+        secondToken = string;
     }
 
     public JsonName() {}
